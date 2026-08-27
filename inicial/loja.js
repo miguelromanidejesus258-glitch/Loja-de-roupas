f438697b38d79db03f10de01738901c40f4c4349
Miguel <miguelromanidejesus258@gmail.com>
2026-08-27 12:14:20 -0300

    novo botão (compre agora), mudei o header e dei inicio ao JS

---
+++ inicial/loja.js
@@ -0,0 +1,10 @@
+function adicionarAoCarrinho(){
+    let carrinho = 0;
+    let produto = "Short Dry fit Nike Masculina";
+    while(carrinho < 10){
+        carrinho++
+        alert(`você adicionou ${carrinho} ${produto} no carrinho`);
+        break;
+    }
+    
+}
